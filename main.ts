7dd98aa4f5137f9289660daa50dc41ce2ce4dc4c
alannguyen00 <58610714+alannguyen00@users.noreply.github.com>
2019-12-20 13:20:24 -0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -79,12 +79,39 @@ namespace myTiles {
 . . . . . . . . . . . . . . . . 
 `
 }
-scene.onHitWall(SpriteKind.Player, function (sprite) {
-    game.over(false)
+scene.onHitTile(SpriteKind.Player, 11, function (sprite) {
+    game.over(true, effects.confetti)
+    // Transforms first sprite image into different image.
+    animation.runImageAnimation(
+    General,
+    [img`
+. . . f f f f f f f f . . . . . 
+. . f f d d d d d d f f . . . . 
+. . f d d 1 1 d 1 1 d f f . . . 
+. . f d d 1 f d f 1 d d f . . . 
+. . d d f d d d d d f d d . . . 
+. . d d f f f f f f f d d . . . 
+. . . d d d d d d d d d . . . . 
+. . . . . d d d d . . . . . . . 
+. . . . . d d d d . . . . . . . 
+. . . . 1 d 1 1 1 1 . . . . . . 
+. . . 1 1 1 2 d 1 1 1 . . . . . 
+. . . d 1 d 1 1 d 1 d . . . . . 
+. . . d 1 1 1 1 d 1 d . . . . . 
+. . . . 8 8 8 8 d 8 . . . . . . 
+. . . . d . . . . 8 . . . . . . 
+. . . d d . . . . 1 d . . . . . 
+`],
+    100,
+    true
+    )
+})
+scene.onHitTile(SpriteKind.Player, 15, function (sprite) {
+    game.over(false, effects.dissolve)
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.object, function (sprite, otherSprite) {
     info.changeScoreBy(1)
-    General.vy += -250
+    General.vy += -300
     Trampoline.setKind(SpriteKind.object)
     Trampoline.x += Math.randomRange(-70, 70)
     Trampoline.y += Math.randomRange(-40, 40)
@@ -94,9 +121,9 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.object, function (sprite, otherS
     [img`
 . . . . f f f f f . . . . . . . 
 . . . f f f f f f f . . . . . . 
-. . f d d d d f f f f . . . . . 
-. . 1 1 d d 1 1 f f f f . . . . 
-. . 1 f d d f 1 f f f f f . . . 
+. . d d d d d f f f f . . . . . 
+. . f 1 d d f 1 f f f f . . . . 
+. . 1 1 d d 1 1 f f f f f . . . 
 . d e e d d d d e e f f f . . . 
 . d d d d d d d e e f d f . f f 
 . f f f f f d d e e f f . f d f 
@@ -110,29 +137,29 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.object, function (sprite, otherS
 . . 8 . f f f f f f f f f . . . 
 `],
     100,
-    true
+    false
     )
 })
 let Trampoline: Sprite = null
 let General: Sprite = null
 scene.setTileMap(img`
-f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
-f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
-f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
+b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b 5 b b b b b b b b b b b b b b b b b b 5 b b b b b 
+b b b b b b b b 5 b b b b b b b b b b b b b b 5 b b b b b b b b b b b b b b 5 b b b b b b b b b b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b 
+b b b b b b 5 b b b b b b b b b b b b b b b b b b b c c c c c b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b 5 b b b b b b c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c b b b b b b b 
+b b b b b b b b c c c b b b b c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c b b b b b b b 
+c c c c c c c c c c c c c c c c c c c c c c c c c c 5 c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c 
+c c c c c 5 c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c 5 c c c c 
 c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c 
-c c c c c c c 5 c c c c c c c c c c c c c c 5 c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c 
-c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c 5 c c c c c c c c c c c c 
-c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c 5 c c c c c c c c c c c c c c c c c c c c c c c c c 
-c c c c c c c c c c c c c c 5 c c c c c c c c c c c c c c c 5 5 c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c 
-c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c 
-c c c c c 5 c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c 
-c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c 
-c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c 5 5 c c c c c c c c c c c c c c c c c c 
-f f f f f f f 5 f f f f f f f f f f f f 5 f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
-f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
-f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
-f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 5 f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
-f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
+c c c c c c c c c c c c c c c c c c c c c c c 5 c c c c c c c c c 5 5 c c c c c c c c c 5 5 c c c c c c 5 c c c c c c c c c c c 
+c c c c c c c c c c c c c c c c c 5 c c c c c c c c c c c c c c c c c 5 c c c c c c c c c c c c c c c c c c c c c c c c c c c c 
+c c c 8 8 8 8 8 c c 5 c c c c c c c c c c c c c c c c c c 5 c c c c c c c c c c c c c c c c c c c c c c c c 5 c c c 5 c c c c c 
+c c 8 8 8 8 8 8 c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c c 
+c 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 c c c c 
+c 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 c c c c 
 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 d d d d d 8 8 8 8 8 8 8 8 8 8 
@@ -142,30 +169,30 @@ f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
 8 8 8 8 8 8 8 8 d d d d d d d d d 8 8 8 8 8 8 8 8 8 8 d d d d d d d d d 8 8 8 8 8 8 8 8 8 8 8 d d d d d d d d d d 8 8 8 8 8 8 8 
 8 8 8 8 8 8 8 8 d d d d d d d d d 8 8 8 8 8 8 8 8 8 8 d d d d d d d d d 8 8 8 8 8 8 8 8 8 8 8 d d d d d d d d d d 8 8 8 8 8 8 8 
 8 8 8 8 8 8 8 8 d d d d d d d d d 8 8 8 8 8 8 8 8 8 8 d d d d d d d d d 8 8 8 8 8 8 8 8 8 8 8 d d d d d d d d d d 8 8 8 8 8 8 8 
-8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 d d d d d d d d d 8 8 8 8 8 8 8 8 8 8 8 8 8 8 d d d d d 8 8 8 8 8 8 8 8 8 
-8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 d d d d d d d d d 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
-8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
-8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 6 8 8 8 8 8 8 8 8 8 8 8 8 8 d d d d d d d d d 8 8 8 8 8 8 8 8 8 8 8 8 8 8 d d d d d 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 6 6 8 8 8 8 8 8 8 8 8 8 8 8 8 d d d d d d d d d 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 6 6 8 8 8 8 8 6 6 6 6 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 6 6 6 6 6 6 6 6 6 6 6 6 6 6 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 8 
 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
-6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
-6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
-6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
-6 6 6 6 6 6 6 6 9 9 9 9 9 9 9 9 9 9 9 9 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 9 9 9 9 9 9 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 6 6 6 6 6 
+6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 6 6 
+6 6 6 6 6 6 6 6 9 9 9 9 9 9 9 9 9 9 9 9 6 6 6 6 6 6 6 6 6 6 6 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 6 6 
 6 6 6 6 6 6 6 6 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
-9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
-9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 5 5 5 5 9 9 9 9 9 5 5 5 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
-9 9 9 9 9 9 9 9 9 9 9 5 5 5 5 9 9 9 9 9 5 5 5 5 5 5 9 5 5 5 5 5 5 9 9 9 5 5 5 5 5 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
-9 9 9 9 7 7 7 7 5 5 5 5 5 5 5 5 7 7 7 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 7 5 5 5 5 5 5 5 9 9 9 5 5 5 5 5 5 9 5 5 5 5 5 5 5 5 5 9 9 9 
-7 7 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 7 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 7 
-7 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 4 5 5 5 5 5 5 5 5 4 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 4 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 5 5 5 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 5 5 5 5 9 9 9 9 9 9 9 9 5 5 5 5 9 9 9 9 9 5 5 5 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 9 5 5 5 5 5 5 9 9 9 5 5 5 5 5 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 9 9 9 5 5 5 5 5 5 9 5 5 5 5 5 5 5 5 5 9 9 9 
+5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 7 
+5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 4 5 5 5 5 5 5 5 5 4 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 4 
 5 5 5 5 5 5 5 5 5 5 5 4 4 4 5 5 5 5 5 5 5 5 4 4 5 5 5 5 4 4 4 4 5 5 5 5 5 5 4 4 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 4 4 4 4 5 5 4 
 5 5 5 5 5 5 5 5 5 4 4 4 4 4 4 5 5 5 5 5 5 4 4 4 4 4 4 4 4 4 4 4 5 5 5 5 5 4 4 4 4 5 5 5 5 5 4 4 4 4 5 5 5 5 5 5 5 4 4 4 4 4 4 4 
 5 5 5 5 5 5 5 4 4 4 4 4 4 4 4 4 5 5 5 5 4 4 4 4 4 4 2 2 2 2 2 4 4 4 4 4 4 2 2 2 2 4 4 4 4 4 4 4 4 4 5 5 5 5 5 5 5 4 4 4 4 4 4 4 
@@ -187,42 +214,42 @@ f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
 // Add extra detail
 //
 // Add extra Adds detail to tiles
-scene.setTile(7, img`
-7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
-7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
-7 7 7 9 9 9 9 9 9 9 9 9 7 7 7 7 
-7 7 9 7 7 7 9 7 7 9 9 7 7 7 7 7 
-7 7 9 7 7 7 9 9 9 9 7 9 7 7 7 7 
-7 7 9 7 7 7 9 7 7 7 7 7 9 7 7 7 
-7 9 7 9 9 9 9 7 7 7 7 7 7 9 7 7 
-7 7 9 9 7 7 9 7 7 7 7 7 7 9 7 7 
-7 7 9 7 9 9 9 9 9 9 9 9 9 9 7 7 
-7 7 9 9 7 7 7 9 7 7 7 7 7 7 7 7 
-7 7 9 9 9 7 7 7 9 9 9 7 7 7 7 7 
-7 7 9 9 9 9 9 7 7 7 9 9 7 7 7 7 
-7 7 7 9 7 7 9 9 9 9 7 7 7 7 7 7 
-7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
-7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
-7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+scene.setTile(15, img`
+5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+5 5 4 4 4 4 4 4 4 4 4 4 4 4 5 5 
+5 5 4 4 4 4 4 4 4 4 4 4 4 4 5 5 
+5 5 4 4 2 2 2 2 2 2 2 2 4 4 5 5 
+5 5 4 4 2 2 5 5 5 2 2 2 4 4 5 5 
+5 5 4 4 2 2 5 4 5 2 2 2 4 4 5 5 
+5 5 4 4 2 2 5 5 5 2 2 2 4 4 5 5 
+5 5 4 4 2 2 2 2 2 2 2 2 4 4 5 5 
+5 5 4 4 2 2 2 2 2 2 2 2 4 4 5 5 
+5 5 4 4 2 2 2 2 2 2 2 2 4 4 5 5 
+5 5 4 4 4 4 4 4 4 4 4 4 4 4 5 5 
+5 5 4 4 4 4 4 4 4 4 4 4 4 4 5 5 
+5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+f f f f f f f f f f f f f f f f 
 `, true)
-scene.setTile(9, img`
-9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
-9 9 9 1 1 1 1 9 9 9 9 9 9 9 9 9 
-9 9 1 1 9 9 1 1 9 9 9 9 9 9 9 9 
-9 1 1 9 9 9 9 1 1 9 9 9 9 1 1 9 
-1 1 9 9 9 9 9 9 1 1 9 1 1 1 1 9 
-9 9 9 9 9 9 9 9 9 1 1 1 9 9 9 9 
-9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
-9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
-9 9 9 1 1 1 1 1 1 9 9 9 9 9 9 9 
-9 9 1 1 9 9 9 9 1 1 9 9 9 9 9 9 
-9 1 1 9 9 9 9 9 9 1 1 9 9 1 1 9 
-9 1 9 9 9 9 9 9 9 9 1 1 1 1 1 9 
-9 1 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
-9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
-9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
-9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
-`, false)
+scene.setTile(11, img`
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b 5 5 5 5 5 b b b b b 
+b b b b b b 5 5 5 5 5 b b b b b 
+b b b b b b 5 5 5 5 5 b b b b b 
+b b b b b b 5 5 5 5 5 b b b b b 
+b b b b b b 5 5 5 5 5 b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+`, true)
 General = sprites.create(img`
 . . . . . . f f f f f f f f f f f f f . . . . . . . . . . . . . 
 . . . . . . . f f f f f f f f f f f f f . . . . . . . . . . . . 
